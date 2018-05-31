6c7cdefbff22ad83b168250132012e448ea9f4f1
alejandronb <anieto@atsistemas.com>
2018-05-31 09:48:34 +0200

    Adding rules for infra-security

--- aws/terraform/01-security.tf
+++ aws/terraform/01-security.tf
@@ -11,7 +11,7 @@ data "aws_security_group" "default" {
 
 resource "aws_security_group" "sg_tf" {
   count          = "${length(var.security_groups)}"
-  name           = "${var.security_groups[count.index]}"
+  name           = "${var.security_groups[count.index]}-${terraform.workspace}"
   description    = "${var.security_groups[count.index]}"
 }
 
@@ -40,4 +40,43 @@ resource "aws_security_group_rule" "master_8443_tf" {
 
   # security_group_id = "${aws_security_group.sg_tf_bastion.id}"
   security_group_id = "${element(aws_security_group.sg_tf.*.id, 1)}"
+}
+
+
+resource "aws_security_group_rule" "infra_8443_tf" {
+  type            = "ingress"
+  from_port       = 8443
+  to_port         = 8443
+  protocol        = "tcp"
+  cidr_blocks     = ["0.0.0.0/0"]
+  # prefix_list_ids = ["pl-12c4e678"]
+
+  # security_group_id = "${aws_security_group.sg_tf_bastion.id}"
+  security_group_id = "${element(aws_security_group.sg_tf.*.id, 2)}"
+}
+
+
+resource "aws_security_group_rule" "infra_443_tf" {
+  type            = "ingress"
+  from_port       = 443
+  to_port         = 443
+  protocol        = "tcp"
+  cidr_blocks     = ["0.0.0.0/0"]
+  # prefix_list_ids = ["pl-12c4e678"]
+
+  # security_group_id = "${aws_security_group.sg_tf_bastion.id}"
+  security_group_id = "${element(aws_security_group.sg_tf.*.id, 2)}"
+}
+
+
+resource "aws_security_group_rule" "infra_80_tf" {
+  type            = "ingress"
+  from_port       = 80
+  to_port         = 80
+  protocol        = "tcp"
+  cidr_blocks     = ["0.0.0.0/0"]
+  # prefix_list_ids = ["pl-12c4e678"]
+
+  # security_group_id = "${aws_security_group.sg_tf_bastion.id}"
+  security_group_id = "${element(aws_security_group.sg_tf.*.id, 2)}"
 }
