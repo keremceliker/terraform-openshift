0465d9989eb75ade99a751c454fa41221ba66539
alejandronb <anieto@atsistemas.com>
2018-05-31 09:54:30 +0200

    Adding disk sizes

--- aws/terraform/main.tf
+++ aws/terraform/main.tf
@@ -79,8 +79,15 @@ resource "aws_instance" "master" {
   # subnet_id         = "${element(aws_subnet.openshift.*.id, 1)}"
   associate_public_ip_address = true
   vpc_security_group_ids = ["${data.aws_security_group.default.id}","${element(aws_security_group.sg_tf.*.id, 1)}"]
+
+  root_block_device {
+    volume_size = "${var.osdisk_size}"
+  }
+
+
   tags {
     Name = "master-tf-${terraform.workspace}"
+    cluster-id = "${var.cluster_id}"
   }
 
 }
@@ -95,8 +102,14 @@ resource "aws_instance" "infra" {
   associate_public_ip_address = true
   vpc_security_group_ids = ["${data.aws_security_group.default.id}","${element(aws_security_group.sg_tf.*.id, 2)}"]
 
+  root_block_device {
+    volume_size = "${var.osdisk_size}"
+  }
+
+
   tags {
     Name = "infra-tf-${terraform.workspace}"
+    cluster-id = "${var.cluster_id}"
   }
 
 }
@@ -112,34 +125,54 @@ resource "aws_instance" "node" {
   # subnet_id         = "${element(aws_subnet.openshift.*.id, 3)}"
   associate_public_ip_address = true
   vpc_security_group_ids = ["${data.aws_security_group.default.id}","${element(aws_security_group.sg_tf.*.id, 3)}"]
-  tags {
-    Name = "node-tf${format("%02d", count.index + 1)}-${terraform.workspace}"
+
+  root_block_device {
+    volume_size = "${var.osdisk_size}"
   }
 
+
+  tags {
+    Name = "node-tf${format("%02d", count.index + 1)}-${terraform.workspace}"
+    cluster-id = "${var.cluster_id}"
+  }
+
+}
+
+
+data "template_file" "node-inventory" {
+  count = "${var.node_count}"
+  template = "${file("templates/hostname.tpl")}"
+  vars {
+    name  = "${aws_instance.node.*.private_dns[count.index]}"
+    ip    = "${aws_instance.node.*.private_ip[count.index]}"
+    # data = "openshift_schedulable=true openshift_node_labels="{'region': 'primary', 'zone': 'east'}""
+  }
 }
 
 
 
 data "template_file" "openshift-inventory" {
   template = "${file("templates/openshift-inventory.tpl")}"
-
   vars {
-    master_hosts   = "${join("\n","${aws_instance.master.*.private_dns}")}"
-    master_url     = "${aws_instance.master.public_dns}"
-    master_ips     = "${join("\n","${aws_instance.master.*.private_ip}")}"
+    master_hosts  = "${join("\n","${aws_instance.master.*.private_dns}")}"
+    master_url    = "${aws_instance.master.public_dns}"
+    master_ips    = "${join("\n","${aws_instance.master.*.private_ip}")}"
     infra_hosts   = "${join("\n","${aws_instance.infra.*.private_dns}")}"
-    infra_ips     = "${join("\n","${aws_instance.infra.*.private_ip}")}"
-    node_hosts   = "${join("\n","${aws_instance.node.*.private_dns}")}"
-    node_ips     = "${join("\n","${aws_instance.node.*.private_ip}")}"
-
+    infra_ips     =  "${join("\n","${aws_instance.infra.*.private_ip}")}"
+    # node_hosts    = "${join("\n","${aws_instance.node.*.private_dns}")}"
+    node_hosts    = "${join(" ",data.template_file.node-inventory.*.rendered)}"
+    # node_hosts    = "${data.template_file.node-inventory.*.rendered}"
+    node_ips     =  "${join("\n","${aws_instance.node.*.private_ip}")}"
+    cluster_id   = "${var.cluster_id}"
   }
 }
-# network_interface_ids            = ["${module.network_interface.id[count.index]}"]
 
 
 #
 # "${element(data.azurerm_subnet.data_subnet.*.id, 1)}"
 
 output "openshift-inventory" {
-        value = "${data.template_file.openshift-inventory.*.rendered}"
+        # value = "${data.template_file.openshift-inventory.*.rendered}"
+        # value = "${data.template_file.node-inventory.*.rendered}"
+        value = "${data.template_file.openshift-inventory.rendered}"
 }
