5075f16bc8963e79dd569929a0167fadc6b41f16
alejandronb <anieto@atsistemas.com>
2018-06-12 20:29:16 +0200

    Minor change

--- aws/terraform/providers.tf
+++ aws/terraform/providers.tf
@@ -13,5 +13,5 @@ variable "aws_secret_key" {
 provider "aws" {
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
-  region     = "${var.zone}"
+  region     = "${var.region}"
 }
