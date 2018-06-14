554d78dae08e3ef6720ba8214bf524736f5cb0e6
alejandronb <anieto@atsistemas.com>
2018-06-14 08:32:30 +0200

    Adding small functionality about workspaces

--- aws/bootstrap.sh
+++ aws/bootstrap.sh
@@ -3,19 +3,27 @@ set -e
 
 cd terraform
 
+WORKSPACE=$(terraform workspace show)
+
+
 echo "Planning Terraform changes..."
-terraform plan -var-file='dev.tfvars' -var-file='aws.tfvars' -out='aws.tfplan'
+# terraform plan -var-file='dev.tfvars' -var-file='aws.tfvars' -out='aws.tfplan'
+# terraform plan -var-file='pre.tfvars' -var-file='aws.tfvars' -out='aws-pre.tfplan'
+terraform plan -var-file=$WORKSPACE.tfvars -var-file='aws.tfvars' -out=aws-$WORKSPACE.tfplan
 
 
-echo "Deploying Terraform plan..."
-terraform apply aws.tfplan
+# echo "Deploying Terraform plan..."
+# terraform apply aws.tfplan
+terraform apply aws-$WORKSPACE.tfplan
+
 
 echo "Creating openshift inventory"
 terraform output openshift-inventory > ../openshift-inventory
 
 echo "Getting output variables..."
 BASTION_IP=$(terraform output bastion_public_ip)
-CONSOLE_URL=$(terraform output master_public_dns)
+# CONSOLE_URL=$(terraform output master_public_dns)
+
 # CONSOLE_IP=$(terraform output master_public_ip)
 
 cd ..
@@ -37,15 +45,6 @@ ssh -t -o StrictHostKeychecking=no -i certs/anieto.pem centos@$BASTION_IP ansibl
 ssh -t -o StrictHostKeychecking=no -i certs/anieto.pem centos@$BASTION_IP ansible-playbook -i /home/centos/openshift-inventory openshift-ansible/playbooks/prerequisites.yml
 ssh -t -o StrictHostKeychecking=no -i certs/anieto.pem centos@$BASTION_IP ansible-playbook -i /home/centos/openshift-inventory openshift-ansible/playbooks/deploy_cluster.yml
 
-
-
-
+CONSOLE_URL=openshift-$WORKSPACE.mithrandir.gq
 echo "Console: https://$CONSOLE_URL:8443"
 echo "Bastion: ssh -i ~/.ssh/anieto.pem centos@$BASTION_IP"
-
-
-
-# echo "Finished!!"
-# echo "Console: https://$CONSOLE_IP:8443"
-# echo "Bastion: ssh -i certs/bastion.key $ADMIN_USER@$BASTION_IP"
-# echo "Router: $SERVICE_IP"
